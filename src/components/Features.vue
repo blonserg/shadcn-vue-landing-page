2fa3174afa0d82db365188f00ea71a6a1eb6ba7e
Sergei Barsukov <sergforweb@gmail.com>
2025-05-05 11:04:03 -0400

    add content

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -21,39 +21,57 @@ interface FeaturesProps {
 const featureList: FeaturesProps[] = [
   {
     icon: "tabletSmartphone",
-    title: "Mobile Friendly",
+    title: "Project managment",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A odio velit cum aliquam, consectetur.",
+      "A highly experienced team who can mange projects with accountability.",
   },
   {
     icon: "badgeCheck",
-    title: "Social Proof",
+    title: "ESTIMATING",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Natus consectetur, odio ea accusamus aperiam.",
+      "We use leading edge software to insure the most precise estimates.",
   },
   {
     icon: "goal",
-    title: "Targeted Content",
+    title: "LOGISTICS & STRATEGIC PLANNING",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. odio ea accusamus aperiam.",
+      "Coordinated planning focused on time management and efficiency.",
   },
   {
     icon: "pictureInPicture",
-    title: "Strong Visuals",
+    title: "LIBRARY OF PRODUCTS",
     description:
-      "Lorem elit. A odio velit cum aliquam. Natus consectetur dolores, odio ea accusamus aperiam.",
+      "Our relationships with vendors has allowed for a vast product library.",
   },
   {
     icon: "mousePointerClick",
-    title: "Clear CTA",
+    title: "LEED CONSULTING",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing. odio ea accusamus consectetur.",
+      "We recommend the optimal products and services for long-term sustainability.",
   },
   {
     icon: "newspaper",
-    title: "Clear Headline",
+    title: "CERTIFIED INSTALLATION",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A odio velit cum aliquam. Natus consectetur.",
+      "Dedicated teams of installers contribute to the success of delivering projects on time.",
+  },
+  {
+    icon: "tabletSmartphone",
+    title: "FLOOR LEVELING & SURVEYING",
+    description:
+      "With over 15 years of experience in both fields, Source One Flooring can take care of all pre-flooring preparations and tests.",
+  },
+  {
+    icon: "badgeCheck",
+    title: "MAINTENANCE",
+    description:
+      "We offer these services in an on-going basis and cover wide range of sectors, including hospitality, corporate, health care and education.",
+  },
+  {
+    icon: "goal",
+    title: "SPECIFICATION & DESIGN SUPPORT",
+    description:
+      "With over 50 years of combined experience ranging from all sectors of the industry, our sales team is ready for any project.",
   },
 ];
 
@@ -80,12 +98,9 @@ const iconMap: Record<
 </script>
 
 <template>
-  <section
-    id="features"
-    class="container py-24 sm:py-32"
-  >
+  <section id="features" class="container py-24 sm:py-32">
     <h2 class="text-lg text-primary text-center mb-2 tracking-wider" data-aos="fade-up">
-      Features
+      Services
     </h2>
 
     <h2 class="text-3xl md:text-4xl text-center font-bold mb-4" data-aos="fade-up">
@@ -93,25 +108,22 @@ const iconMap: Record<
     </h2>
 
     <h3 class="md:w-1/2 mx-auto text-xl text-center text-muted-foreground mb-8" data-aos="fade-up">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-      fugiat, odit similique quasi sint reiciendis quidem iure veritatis optio
-      facere tenetur.
+      Source One Floorings is an innovative provider of commercial flooring solution in the DC Metro area. Our vision is
+      to redefine the flooring industry standards by delivering the highest level of contentment to our clients. Source
+      One Flooring is committed to its founding principals of establishing relationships, delivering exceptional
+      workmanship and Strategic planning. Our experienced team of professionals will provide intuitive expert advice,
+      dependability and open assertive communication. The team meticulously pays the same level of attention to detail
+      no matter how big and complex or small and simple the project.
+
+      Source One Flooring is proud to be recognized as one of the new leaders in the Flooring industry.
     </h3>
 
     <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      <div
-        v-for="{ icon, title, description } in featureList"
-        :key="title"
-      >
+      <div v-for="{ icon, title, description } in featureList" :key="title">
         <Card class="h-full bg-background border-0 shadow-none" data-aos="fade-up">
           <CardHeader class="flex justify-center items-center">
-            <div
-              class="bg-primary/20 p-2 rounded-full ring-8 ring-primary/10 mb-4"
-            >
-              <component
-                :is="iconMap[icon]"
-                class="size-6 text-primary"
-              />
+            <div class="bg-primary/20 p-2 rounded-full ring-8 ring-primary/10 mb-4">
+              <component :is="iconMap[icon]" class="size-6 text-primary" />
             </div>
 
             <CardTitle>
